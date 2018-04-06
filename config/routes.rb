2b73810c481f47aa31ff4d3c8eb137149265d88d
@smart-web-rock <faonestar514@gmail.com>
2018-04-06 06:24:25 -0700

    create new template

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'patient/index'
+  get 'patient/personinfo'
 
   get 'home/index'
   root :to=> 'home#index'
   get 'home/personal'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
